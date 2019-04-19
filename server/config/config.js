243345e8adb5f92a46ea990ddd9e8a8ceb1c2362
Keenan Van Blerk <keenanvanblerk@gmail.com>
2019-04-19 12:13:31 +0200

    update config

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,14 @@
-var env = process.env.NODE_ENV || 'developement';
+var env = process.env.NODE_ENV || 'development';
 
-if(env === 'developement' || env === 'test'){
+if (env === 'development' || env === 'test') {
     var config = require('./config.json');
     var envConfig = config[env];
-    Object.keys(envConfig).forEach((val)=>{
+    Object.keys(envConfig).forEach((val) => {
         process.env[val] = envConfig[val];
     });
 }
 
-// if(env === 'developement'){
+// if(env === 'development'){
 //     process.env.PORT = 3000;
 //     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
 // }else if(env === 'test'){
